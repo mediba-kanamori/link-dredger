b7f22c6ed5cf4e433896ad990afcb09902919f2b
uknmr <y.kinmori@gmail.com>
2016-02-16 01:26:03 +0900

    Options pageを作成

---
+++ options.js
@@ -0,0 +1,10 @@
+'use strict'
+
+document.addEventListener('DOMContentLoaded', () => {
+  console.log('restore');
+});
+
+
+document.getElementById('save').addEventListener('click', () => {
+  console.log('saved');
+});
